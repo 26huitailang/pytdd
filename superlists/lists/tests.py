0992023787e08175cce4939d144a763cd22511ee
26huitailang <26huitailang@gmail.com>
2019-06-14 16:42:24 +0800

    作弊不能通过更严格的测试

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+import re
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
@@ -7,14 +8,30 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r"<input[^>]+csrfmiddlewaretoken[^>]+>"
+        return re.sub(csrf_regex, "", html_code)
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
+        found = resolve("/")
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string("home.html")
+        self.assertEqual(self.remove_csrf(response.content.decode()), expected_html)
 
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = "A new list item"
+
+        response = home_page(request)
+        self.assertIn("A new list item", response.content.decode())
+        expected_html = render_to_string(
+            "home.html", {"new_item_text": "A new list item"}
+        )
+        content = self.remove_csrf(response.content.decode())
+        self.assertEqual(content, expected_html)
