c64ed3f0cccef1444e03251472ef51a9dffbfaa8
26huitailang <26huitailang@gmail.com>
2019-06-14 18:12:11 +0800

    Redirect after POST, and show all items in template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -6,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(20)
 
     def tearDown(self):
         self.browser.quit()
@@ -37,8 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        self.browser.implicitly_wait(1)
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
         self.check_for_row_in_list_table("1: Buy peacock feathers")
@@ -49,6 +50,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         # 页面再次更新，清单中显示了这两个待办事项
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
