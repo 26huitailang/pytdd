c7df91a14c009d219a6f9cdc5ffdce894a48cb87
26huitailang <26huitailang@gmail.com>
2019-06-15 10:46:05 +0800

    隔离清单的功能测试

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -35,13 +35,12 @@ class NewVisitorTest(LiveServerTestCase):
         # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
         inputbox.send_keys("Buy peacock feathers")
 
-        # 她按回车键后，页面更新了
+        # 她按回车键后，被带到一个新URL
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
+        edith_lists_url = self.browser.current_url
+        self.assertRegex(edith_lists_url, '/lists/.+')
         time.sleep(1)
-
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # 页面中还有一个文本框，可以输入其他的待办事项
@@ -53,16 +52,40 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
         # 页面再次更新，清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
-        # 伊迪丝想知道这个网站是否会记住她的清单
-        # 她看到网站为她生成了一个唯一的URL
-        # 页面中有一些文字解说这个功能
-        self.fail("Finish the test!")
+        # 现在一个叫作弗朗西斯的新用户访问了网站
 
-        # 她访问那个URL，发现待办事项清单还在
-        # 页面再次更新，她的清单中显示了这两个待办事项
+        ## 我们使用一个新浏览器会话
+        ## 确保伊迪丝的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 弗朗西斯输入一个新待办事项，新建一个清单
+        # 他不像伊迪丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertNotEqual(francis_list_url, edith_lists_url)
+
+        # 这个页面还是没有伊迪丝的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 两人都很满意，去睡觉了
+        self.fail("Finish the test!")
 
