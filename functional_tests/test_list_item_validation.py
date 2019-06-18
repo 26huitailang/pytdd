e0d07c8721335ef0187e1c5e6a448929511f5db8
26huitailang <26huitailang@gmail.com>
2019-06-18 14:15:13 +0800

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,5 @@
 from .base import FunctionalTest
-from unittest import skip
+from selenium.webdriver.common.keys import Keys
 
 
 class ItemValidationTest(FunctionalTest):
@@ -7,7 +7,7 @@ class ItemValidationTest(FunctionalTest):
         # 伊迪丝访问首页，不小心提交了一个空待办事项
         # 输入框中没输入内容，她就按下了回车键
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id("id_new_item").send_keys("\n")
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
 
         # 首页刷新了，显示一个错误信息
         # 提示待办事项不能为空
@@ -15,11 +15,12 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # 她输入一些文字，然后再次提交，这次没问题了
-        self.browser.find_element_by_id("id_new_item").send_keys("Buy milk\n")
+        self.browser.find_element_by_id("id_new_item").send_keys("Buy milk")
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
         self.check_for_row_in_list_table("1: Buy milk")
 
         # 她有点儿调皮😝，又提交了一个空待办事项
-        self.browser.find_element_by_id("id_new_item").send_keys("\n")
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
 
         # 在清单页面她看到了一个类似的错误信息
         self.check_for_row_in_list_table("1: Buy milk")
