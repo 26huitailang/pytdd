8955295bccc8a9b74375054b19f8bc7261bdfdbe
26huitailang <26huitailang@gmail.com>
2019-06-18 15:50:32 +0800

    new form for list items

--- lists/models.py
+++ lists/models.py
@@ -3,11 +3,10 @@ from django.urls import reverse
 
 
 class List(models.Model):
-
     def get_absolute_url(self):
-        return reverse('view_list', args=[self.id])
+        return reverse("view_list", args=[self.id])
 
 
 class Item(models.Model):
-    text = models.TextField(default='')
+    text = models.TextField(default="")
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
