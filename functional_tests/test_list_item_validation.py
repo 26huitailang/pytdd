52162d01eb8eb9c9119d21739a90faacbfc763ae
26huitailang <26huitailang@gmail.com>
2019-06-19 13:49:50 +0800

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,8 +13,8 @@ class ItemValidationTest(FunctionalTest):
 
         # 首页刷新了，显示一个错误信息
         # 提示待办事项不能为空
-        error = self.browser.find_element_by_css_selector(".has-error")
-        self.assertEqual(error.text, "You can't have an empty list item")
+        self.browser.find_element_by_css_selector("#id_text:invalid")
+        # self.assertEqual(error.text, "You can't have an empty list item")
 
         # 她输入一些文字，然后再次提交，这次没问题了
         self.get_item_input_box().send_keys("Buy milk")
@@ -27,11 +27,29 @@ class ItemValidationTest(FunctionalTest):
 
         # 在清单页面她看到了一个类似的错误信息
         self.check_for_row_in_list_table("1: Buy milk")
-        error = self.browser.find_element_by_css_selector(".has-error")
-        self.assertEqual(error.text, "You can't have an empty list item")
+        # error = self.browser.find_element_by_css_selector(".has-error")
+        self.browser.find_element_by_css_selector("#id_text:invalid")
+        # self.assertEqual(error.text, "You can't have an empty list item")
 
         # 输入文字之后就没问题了
         self.get_item_input_box().send_keys("Make tea")
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.check_for_row_in_list_table("1: Buy milk")
         self.check_for_row_in_list_table("2: Make tea")
+
+    def test_cannot_add_duplicate_items(self):
+        # 伊迪丝访问首页，新建一个清单
+        self.browser.get(self.server_url)
+        inputbox = self.get_item_input_box()
+        inputbox.send_keys('Buy wellies')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy wellies')
+
+        # 她不小心输入了一个重复的待办事项
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # 她看到一条有帮助的错误信息
+        self.check_for_row_in_list_table('1: Buy wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
