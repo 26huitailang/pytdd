4fb8d37f17008e27a1e4d17e13c4bd3d0a1328f4
26huitailang <26huitailang@gmail.com>
2019-06-28 13:41:11 +0800

    new_list view tries to assign owner but cannot

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
 from django.contrib.sessions.backends.db import SessionStore
+from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
 from .server_tools import create_session_on_server
 from .management.commands.create_session import create_pre_authenticated_session
@@ -24,11 +25,38 @@ class MyListsTest(FunctionalTest):
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         email = "edith@example.com"
 
-        self.browser.get(self.server_url)
-        self.wait_to_be_logged_out(email)
-
         # 伊迪丝是已登陆用户
         self.create_pre_authenticated_session(email)
 
         self.browser.get(self.server_url)
-        self.wait_to_be_logged_in(email)
+        self.get_item_input_box().send_keys('Reticualte splines')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Immanentize eschaton')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        first_list_url = self.browser.current_url
+
+        # 她第一次看到My Lists链接
+        self.browser.find_element_by_link_text('My Lists').click()
+
+        # 她看到这个页面中有她创建的清单
+        # 而且清单根据第一个待办事项命名
+        self.browser.find_element_by_link_text('Reticualte splines').click()
+        self.assertEqual(self.browser.current_url, first_list_url)
+
+        # 她决定再建一个清单试试
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Click cows')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        second_list_url = self.browser.current_url
+
+        # 在My Lists页面，这个新建的清单也显示出来了
+        self.browser.find_element_by_link_text('My Lists').click()
+        self.browser.find_element_by_link_text('Click cows').clikc()
+        self.assertEqual(self.browser.current_url, second_list_url)
+
+        # 她退出后，My Lists链接不见了
+        self.browser.find_element_by_id('id_logout').click()
+        self.assertEqual(
+            self.browser.find_element_by_link_text('My Lists'),
+            []
+        )
